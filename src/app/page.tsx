cd28679090526b622a99d22a8e1e82151ddf473d
Muhammad Salman Ansar <salmanwattoo79@gmail.com>
2025-05-01 14:27:20 +0500

    Added sticy scroll effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "./components/HeroSection";
 import FeaturesCourses from "./components/FeaturesCourses";
+import WhyChooseUs from "./components/WhyChooseUs";
 export default function Home() {
   return (
     <>
       <main className="min-h-screen">
         <HeroSection />
         <FeaturesCourses />
+        <WhyChooseUs />
       </main>
     </>
   );
